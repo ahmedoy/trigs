3b741013ca30463cff7c880693f68a83e22fa060
ahmedoy <ahmedoy34@gmail.com>
2024-10-22 12:28:15 +0300

    Added STN to VGGMOD and moved to custom_models

--- trigs/custom_models/__init__.py
+++ trigs/custom_models/__init__.py
@@ -1,3 +1,4 @@
 from .vgg_tin import VGG16TIN
 from .flex_resnet import FlexResNet, baseline_classifier, resnet18_mod
 from .hybrid_vit import hybrid_transfomer, HybridTranformer
+from .vgg_mod_cifar10 import VGG_MOD
